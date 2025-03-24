7c5bff08bf3b2aba22e7a38a2c80dbf26f24d09b
romaopedro199 <romaopedro199@gmail.com>
2025-03-24 13:07:44 -0300

    add code splitting

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,20 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PublicRoute from "@/routes/PublicRoute";
 import PrivateRoute from "@/routes/PrivateRoute";
+import { lazy, Suspense } from "react";
+
+const SignIn = lazy(() => import("@/modules/auth/SignIn"));
+const SignUp = lazy(() => import("@/modules/auth/SignUp"));
+const Products = lazy(() => import("@/modules/products/Products"));
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<PublicRoute />}>
-          <Route path="/" element={<div>Login</div>} />
-          <Route path="/login" element={<div>Login</div>} />
-          <Route path="/signup" element={<div>Signup</div>} />
-        </Route>
-        <Route element={<PrivateRoute />}>
-          <Route path="/products" element={<div>Private Route</div>} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/signIn" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/products" element={<Products />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
