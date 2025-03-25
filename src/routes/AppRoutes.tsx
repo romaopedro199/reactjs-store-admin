be6103a0d559cc1af71f673a9b2d4b486d9fd0e2
romaopedro199 <romaopedro199@gmail.com>
2025-03-25 11:17:09 -0300

    create 1 file, delete 1 file and update 1 file

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,7 +5,7 @@ import { lazy, Suspense } from "react";
 
 const SignIn = lazy(() => import("@/modules/auth/SignIn"));
 const SignUp = lazy(() => import("@/modules/auth/SignUp"));
-const Products = lazy(() => import("@/modules/products/Products"));
+const Dashboard = lazy(() => import("@/modules/dashboard/Dashboard"));
 
 const AppRoutes = () => {
   return (
@@ -18,7 +18,7 @@ const AppRoutes = () => {
             <Route path="/signup" element={<SignUp />} />
           </Route>
           <Route element={<PrivateRoute />}>
-            <Route path="/products" element={<Products />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Route>
         </Routes>
       </Suspense>
