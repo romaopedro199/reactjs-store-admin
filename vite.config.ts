06be4f89e72fcaaa168e5785394acb3e3fd47c69
romaopedro199 <romaopedro199@gmail.com>
2025-04-03 22:40:19 -0300

    add viteCompression

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 // @ts-expect-error - Temporarily ignoring missing declaration file for 'path'.
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   resolve: {
     alias: {
       // @ts-expect-error - Temporarily ignoring missing declaration file for 'path'.
